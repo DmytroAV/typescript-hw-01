c7142f273b9e93d6ac0beda1fe1bc6367ea68050
Dmitriy <velkov55@gmail.com>
2023-12-03 09:52:58 +0200

    added tsc-hw-1

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(1, 1);
+console.log('add(1, 1) :>> ', add(1, 1));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+add(1, 1);
+console.log('add(1, 1) :>> ', add(1, 1));
